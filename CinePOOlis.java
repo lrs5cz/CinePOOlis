af5ce8beb9712088bf99998ceac8e1bd1bcae03a
lrs5cz <lrs5cz@gmail.com>
2025-11-11 17:55:33 -0600

    Estructura inicial de main

---
+++ CinePOOlis.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CinePOOlis {
+
+    public static void iniciarSesion() {
+
+    }
+
+    public static void menu () {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
