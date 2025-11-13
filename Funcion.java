fa61b7ff0768e350c0712a5599f2a5df7d9576f4
lrs5cz <lrs5cz@gmail.com>
2025-11-12 20:19:15 -0600

    Enhance Funcion class with seating methods
    Added methods to obtain seating arrangements and convert row numbers to letters.

--- Funcion.java
+++ Funcion.java
@@ -44,6 +44,66 @@ public class Funcion {
         this.sala = sala;
     }
 
+    // Función para convertir un valor numérico de fila a letra
+    public String obtenerLetraFila(int fila) {
+        return String.valueOf(letra);
+        char letra = (char) ('A' + fila); // Convierte el número de fila a letra (0 -> A, 1 -> B, etc.)
+    }
+
+    public int obtenerNumeroFila(String letra) {
+        return letra.charAt(0) - 'A'; // Convierte la letra de fila a número (A -> 0, B -> 1, etc.)
+    }
+
+    
+    // Función para obtener los asientos de la sala, así como su disponibilidad
+    public String[][] obtenerAsientosA() {
+        String[][] asientosA = new String[10][15];
+        for (int i = 0; i < asientosA.length; i++) {
+            String fila = obtenerLetraFila(i);
+            for (int j = 0; j < asientosA[i].length; j++) {
+                int columna = j + 1;
+                asientosA[i][j] = fila + columna + "[O]"; // O representa un asiento disponible
+            }
+        }
+        return asientosA;
+    }
+
+    public String[][] obtenerAsientosB() {
+    String[][] asientosB = new String[10][15];
+    for (int i = 0; i < asientosB.length; i++) {
+        String fila = obtenerLetraFila(i);
+
+        for (int j = 0; j < asientosB[i].length; j++) {
+
+            boolean esFilaPasillo = (i >= 0 && i <= 3); 
+            
+            boolean esPasilloIzquierdo = (j >= 0 && j <= 3); 
+            
+            boolean esPasilloDerecho = (j >= 11 && j <= 14); 
+
+            if (esFilaPasillo && (esPasilloIzquierdo || esPasilloDerecho)) {
+                asientosB[i][j] = "    "; 
+            } else {
+                int columna = j + 1;
+                asientosB[i][j] = fila + columna + "[O]"; // O representa un asiento disponible
+            }
+        }
+    }
+    return asientosB;
+}
+
+    public String[][] obtenerAsientosVIP() {
+        String[][] asientosVIP = new String[8][6];
+        for (int i = 0; i < asientosA.length; i++) {
+            String fila = obtenerLetraFila(i);
+            for (int j = 0; j < asientosVIP[i].length; j++) {
+                int columna = j + 1;
+                asientosVIP[i][j] = fila + columna + "[O]"; // O representa un asiento disponible
+            }
+        }
+        return asientosVIP;
+    }
+
     // Genera un ID de la función
     @Override
     public String toString() {
