62f6454c67d2f4fb1bad68fca823b26ed74fe0c2
lrs5cz <lrs5cz@gmail.com>
2025-11-23 16:57:05 -0600

    Refactor Funcion class with new attributes and methods
    Updated attributes and constructor to include 'nombrePelicula'. Added a method to check room availability.

--- Funcion.java
+++ Funcion.java
@@ -1,10 +1,12 @@
 public class Funcion {
     // Atributos
-    String idPelicula, fecha, hora, sala;
+    String nombrePelicula, idPelicula, fecha, hora, sala;
     Pelicula pelicula;
 
     // Constructor
-    public Funcion(String idPelicula, String fecha, String hora, String sala) {
+    public Funcion(Pelicula pelicula, String fecha, String hora, String sala) {
+        this.pelicula = pelicula;
+        this.nombrePelicula = pelicula.getNombrePelicula;
         this.idPelicula = pelicula.generarIdPelicula();
         this.fecha = fecha;
         this.hora = hora;
@@ -12,6 +14,14 @@ public class Funcion {
     }
 
     // Getters y Setters
+    public String getNombrePelicula() {
+        return nombrePelicula;
+    }
+
+    public void setNombrePelicula(String nombrePelicula) {
+        this.nombrePelicula = nombrePelicula;
+    }
+
     public String getIdPelicula() {
         return idPelicula;
     }
@@ -69,28 +79,28 @@ public class Funcion {
     }
 
     public String[][] obtenerAsientosB() {
-    String[][] asientosB = new String[10][15];
-    for (int i = 0; i < asientosB.length; i++) {
-        String fila = obtenerLetraFila(i);
+        String[][] asientosB = new String[10][15];
+        for (int i = 0; i < asientosB.length; i++) {
+            String fila = obtenerLetraFila(i);
 
-        for (int j = 0; j < asientosB[i].length; j++) {
+            for (int j = 0; j < asientosB[i].length; j++) {
 
-            boolean esFilaPasillo = (i >= 0 && i <= 3); 
-            
-            boolean esPasilloIzquierdo = (j >= 0 && j <= 3); 
-            
-            boolean esPasilloDerecho = (j >= 11 && j <= 14); 
+                boolean esFilaPasillo = (i >= 0 && i <= 3); 
+                
+                boolean esPasilloIzquierdo = (j >= 0 && j <= 3); 
+                
+                boolean esPasilloDerecho = (j >= 11 && j <= 14); 
 
-            if (esFilaPasillo && (esPasilloIzquierdo || esPasilloDerecho)) {
-                asientosB[i][j] = "    "; 
-            } else {
-                int columna = j + 1;
-                asientosB[i][j] = fila + columna + "[O]"; // O representa un asiento disponible
+                if (esFilaPasillo && (esPasilloIzquierdo || esPasilloDerecho)) {
+                    asientosB[i][j] = "    "; 
+                } else {
+                    int columna = j + 1;
+                    asientosB[i][j] = fila + columna + "[O]"; // O representa un asiento disponible
+                }
             }
         }
+        return asientosB;
     }
-    return asientosB;
-}
 
     public String[][] obtenerAsientosVIP() {
         String[][] asientosVIP = new String[8][6];
@@ -102,6 +112,11 @@ public class Funcion {
             }
         }
         return asientosVIP;
+    }
+
+    // Función para comprobar si una sala tiene disponibilidad
+    public boolean disponibilidadSala(String[][] asientos) {
+        return true;
     }
 
     // Genera un ID de la función
