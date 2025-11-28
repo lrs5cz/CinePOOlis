7ff2cfea21d1f6c66ae2de4f1fbcc2111a5d98aa
lrs5cz <lrs5cz@gmail.com>
2025-11-28 16:51:39 -0600

    Delete Almacenamiento.java

--- Almacenamiento.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.util.List;
-
-public interface Almacenamiento {
-    // Métodos para guardar y cargar datos
-    void guardarDatos(List<String> datos);
-    List<String> cargarDatos() throws IOException;
-}
