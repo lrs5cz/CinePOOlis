3cb84346a5b6787e8333b6aac2cc09a0f2f52235
natalieherrera33 <nataherrera.0303@gmail.com>
2025-11-30 13:35:54 -0600

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -1,11 +1,13 @@
 public class Empleado extends Persona {
     // Atributos 
     private String turno;
+    private String rol;//nat
 
     // Constructor
-    public Empleado(String nombre, String apellidoP, String apellidoM, int edad, String numeroCelular, Cuenta cuenta, String turno) {
+    public Empleado(String nombre, String apellidoP, String apellidoM, int edad, String numeroCelular, Cuenta cuenta, String turno,String rol) {//nat
         super(nombre, apellidoP, apellidoM, edad, numeroCelular, cuenta);
         this.turno = turno;
+        this.rol = rol;//nat
     }
 
     // Getters y Setters
@@ -15,4 +17,13 @@ public class Empleado extends Persona {
     public void setTurno(String turno) {
         this.turno = turno;
     }   
+
+    //nat
+    public String getRol(){
+        return rol;
+    }
+    public void setRol(String rol){
+        this.rol=rol;
+    }
+
 }
